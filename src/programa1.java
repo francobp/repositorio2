8241e0658c7d9126f22b160c3abe6cbc5b6787f6
franco burbano <franco.009@hotmail.com>
2013-08-06 12:25:50 -0500

    inicio

---
+++ src/programa1.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+
+public class programa1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+JOptionPane.showInputDialog("el bitchel");
+	}
+
+}
