70240e8719245ce86737e99d6018bf4d05cb65c2
Michel Vargas <micheldorian91@gmail.com>
2013-08-06 12:37:53 -0500

    la pregunta de franco

--- src/programa1.java
+++ src/programa1.java
@@ -8,7 +8,7 @@ public class programa1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-JOptionPane.showInputDialog("el bitchel");
+JOptionPane.showInputDialog("FRANCO ES GAY");
 	}
 
 }
